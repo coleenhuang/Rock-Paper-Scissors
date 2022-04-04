a8611f5280f1c33c403a07021acbc6ff6a4535e9
coleenhuang <coleenhuang@gmail.com>
2022-04-04 13:03:15 -0700

    implemented tie in playround function

--- app.js
+++ app.js
@@ -2,5 +2,23 @@ const optionsArray = ["rock", "paper", "scissors"]
 function computerPlay(myArray) {
     return myArray[Math.floor(Math.random()* myArray.length)]
 }
-console.log(computerPlay(optionsArray))
+
+function playRound (playerSelection, computerSelection) {
+    //Compares computer and player input to see who wins
+    if (playerSelection === computerSelection) {
+        return "Tie"
+    }
+    else {
+        return "hey!"
+    }
+}
+const playerSelection = "rock";
+const computerSelection = computerPlay(optionsArray);
+console.log(playRound(playerSelection, computerSelection))
+
+
+function game(){
+    //Keeps score and calculates who wins or loses
+    //call playRound function within this function
+}
 
